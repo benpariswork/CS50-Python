47e87543600d4d9d78c298472c5aea5356790a0c
benpariswork <88748300+benpariswork@users.noreply.github.com>
2024-05-26 13:57:32 -0700

    refine calc.py
    shortens code, same results

--- lecture-0/calc.py
+++ lecture-0/calc.py
@@ -1,10 +1,7 @@
-# Take inputs for x and y
-x = input("What's x? ")
-y = input("What's y? ")
+# Take inputs for x and y as integers
+x = int(input("What's x? "))
+y = int(input("What's y? "))
 
-# Define z as x+y
-z = int(x) + int(y)
-
-# Print z
-print(z)
+# Print x + y
+print(x + y)
 
