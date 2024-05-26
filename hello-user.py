9d5e5bdf25cb3acf74963e09b6962ca9f8e96926
benpariswork <88748300+benpariswork@users.noreply.github.com>
2024-05-26 13:10:42 -0700

    updated comments on hello-user.py

--- hello-user.py
+++ hello-user.py
@@ -1,5 +1,10 @@
 #Takes user's name as input and prints hello and the user name
 
+#Ask user for their name
 name = input("What is your name?")
+
+#Print hello,
 print("hello, ")
+
+#Print name
 print (name)
