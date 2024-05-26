5259350f22c351f6b458946d73f63835afe9676d
benpariswork <88748300+benpariswork@users.noreply.github.com>
2024-05-26 13:21:46 -0700

    update hello-user.py

--- lecture-0/hello-user.py
+++ lecture-0/hello-user.py
@@ -1,7 +1,7 @@
 #Takes user's name as input and prints hello and the user name
 
 #Ask user for their name
-name = input("What is your name?")
+name = input("What is your name? ")
 
 #Print hello, NAME
-print("hello,", name)
+print(f"hello, {name}")
