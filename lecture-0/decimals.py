eeb1b3313552f23d4ee4e5a85be950552f555ca0
benpariswork <88748300+benpariswork@users.noreply.github.com>
2024-05-27 12:37:59 -0700

    decimal rounding schemes

---
+++ lecture-0/decimals.py
@@ -0,0 +1,13 @@
+# Get the user's input
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+# Calculate the result
+z = round(x / y, 2)
+
+# Print the result
+print(z)
+
+
+##alt decimal rounding scheme
+#print(f"{z:.2f}")
