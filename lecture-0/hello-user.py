ad01cb5c3f75da7d52540279680617e4607453f4
benpariswork <88748300+benpariswork@users.noreply.github.com>
2024-05-27 16:46:44 -0700

    added functions

--- lecture-0/hello-user.py
+++ lecture-0/hello-user.py
@@ -1,8 +1,23 @@
 # Takes user's name as input and prints hello and the user name
 
-# Ask user for their name
-# Strip start/end whitespace from string name and apply title case
-name = input("What is your name? ").strip().title()
+# # Ask user for their name
+# # Strip start/end whitespace from string name and apply title case
+# name = input("What is your name? ").strip().title()
 
-# Print hello, NAME
-print(f"hello, {name}")
+# # Print hello, NAME
+# print(f"hello, {name}")
+
+def main():
+
+    #output using custom function
+    name = input("What's your name? ").strip().title()
+    hello(name)
+
+    #output with default 'to' variable
+    hello()
+
+#Custom function to say hello
+def hello(to="world"):
+    print("hello,", to)
+
+main()
