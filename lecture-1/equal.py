358402e00d984bc02339ef2f3fd72a76f08ea5bd
benpariswork <88748300+benpariswork@users.noreply.github.com>
2024-05-27 23:29:30 -0700

    Update equal.py

--- lecture-1/equal.py
+++ lecture-1/equal.py
@@ -3,7 +3,7 @@ x = int(input("What is x? "))
 y = int(input("What is y? "))
 
 # check if x is equal to y
-if x < y or x > y:
+if x != y:
     print("x is not equal to y")
 else:
     print("x is equal to y")
