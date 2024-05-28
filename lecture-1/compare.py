2d217cb15e1aee86338484a2ef2313eabc5d04f7
benpariswork <88748300+benpariswork@users.noreply.github.com>
2024-05-27 17:14:14 -0700

    create compare.py

---
+++ lecture-1/compare.py
@@ -0,0 +1,11 @@
+# take inputs for x and y
+x = int(input("What is x? "))
+y = int(input("What is y? "))
+
+# compare x to y and print result
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")
