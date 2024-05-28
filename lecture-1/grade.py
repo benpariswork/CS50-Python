37e802e28f114a755e6c2c4132f4ed8cf960b7ce
benpariswork <88748300+benpariswork@users.noreply.github.com>
2024-05-27 23:35:37 -0700

    move and update grade.py

---
+++ lecture-1/grade.py
@@ -0,0 +1,15 @@
+#set var score to input
+score = int(input("Score: "))
+
+#output grade based on var score
+  if 90 <= score <= 100:
+      print("Grade: A")
+  elif 80 <= score < 90:
+      print("Grade: B")
+  elif 70 <= score < 80:
+      print("Grade: C")
+  elif 60 <= score < 70:
+      print("Grade: D")
+  else:
+      print("Grade: F")
+
