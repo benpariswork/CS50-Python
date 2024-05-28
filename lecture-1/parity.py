e8a656dd3eb79e87068a4fa43393f9b24e278ed7
benpariswork <88748300+benpariswork@users.noreply.github.com>
2024-05-28 00:11:57 -0700

    Update parity.py

--- lecture-1/parity.py
+++ lecture-1/parity.py
@@ -5,7 +5,7 @@ def main():
     # take input for x as integer
     x = int(input("This program checks if a number is even or odd. Please provide a number to be checked: "))
     #call is_even function and pass x as param
-    if is_even(x) is True:
+    if is_even(x):
         print("Even")
     else:
         print("Odd")
