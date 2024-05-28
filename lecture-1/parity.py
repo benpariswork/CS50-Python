9463a1fb4c5b76428579d1c00786d43ff4e47989
benpariswork <88748300+benpariswork@users.noreply.github.com>
2024-05-28 00:05:06 -0700

    custom function for parity

--- lecture-1/parity.py
+++ lecture-1/parity.py
@@ -1,7 +1,19 @@
 # test input to see if it is even or odd
-x = int(input("What is x? "))
 
-if x % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+# define main function
+def main():
+    # take input for x as integer
+    x = int(input("This program checks if a number is even or odd. Please provide a number to be checked: "))
+    #call is_even function and pass x as param
+    if is_even(x) is True:
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+main()
