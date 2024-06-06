4c137395e210a52c4643578ae063f938a4742e50
benpariswork <88748300+benpariswork@users.noreply.github.com>
2024-06-06 12:06:30 -0700

    fin lect 4

--- lecture-4/name.py
+++ lecture-4/name.py
@@ -1,6 +1,10 @@
 import sys
 
-try:
-    print("hello, my name is", sys.argv[1], sys.argv[2])
-except IndexError:
-    print("Too few arguments")
+import sys
+
+if len(sys.argv) < 3:
+    sys.exit("Too few arguments")
+elif len(sys.argv) > 3:
+    sys.exit("Too many arguments")
+
+print("hello, my name is", sys.argv[1], sys.argv[2])
